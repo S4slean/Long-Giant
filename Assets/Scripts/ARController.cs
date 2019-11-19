99307ff9b61162d48b9734eb96bab8653759a5ff
Dilink <dilink4@gmail.com>
2019-11-19 17:08:16 +0100

    Plane recognition

---
+++ Assets/Scripts/ARController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+
+#if UNITY_EDITOR
+    // Set up touch input propagation while using Instant Preview in the editor.
+    using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class ARController : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    private bool m_IsQuitting = false;
+
+    public void Awake()
+    {
+        // Enable ARCore to target 60fps camera capture frame rate on supported devices.
+        // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+        Application.targetFrameRate = 60;
+    }
+    public void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Should not handle input if the player is pointing on UI.
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            if ((hit.Trackable is DetectedPlane) &&
+                    Vector3.Dot(playerCamera.transform.position - hit.Pose.position,
+                        hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                cube.transform.position = hit.Pose.position;
+
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                gameObject.transform.SetParent(anchor.transform);
+            }
+        }
+    }
+
+    // Create a post at a position
+    // Pose pose = new Pose(position, rotation);
+    // Anchor anchor = plane.CreateAnchor(pose);
+    // gameObject.transform.SetParent(anchor.transform);
+
+    private void _UpdateApplicationLifecycle()
+    {
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to
+        // appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage(
+                "ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
---
+++ Assets/Scripts/LongGiant/Physics/PhysicalObjectScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicalObjectScript : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Rigidbody objectBody = default;
+
+    [Header("Interactions Parameters")]
+    [SerializeField] PhysicalObjectInteractionsType physicalObjectInteractionsType = PhysicalObjectInteractionsType.DealAndReceiveDamages;
+    [SerializeField] float minimumForceToBeDestroyed = 25;
+    [SerializeField] float objectMass = 1;
+    public float GetObjectMass { get { return objectMass; } }
+
+    bool setedUp;
+    public void SetUp()
+    {
+        if (setedUp)
+            return;
+
+        setedUp = true;
+
+        if (objectBody == null)
+            objectBody = GetComponent<Rigidbody>();
+
+        objectBody.mass = objectMass;
+    }
+
+    private void Start()
+    {
+        SetUp();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PhysicalObjectScript hitPhysicalObject = collision.collider.GetComponent<PhysicalObjectScript>();
+        if (hitPhysicalObject != null)
+            GameManager.gameManager.CollisionsManager.AskForCollisionTreatment(this, hitPhysicalObject, collision.relativeVelocity);
+        else
+            CheckForDestroy(collision.relativeVelocity.magnitude * objectMass);
+    }
+
+    public bool CheckForDestroy(float speedForce)
+    {
+        bool destroyed = false;
+
+        Debug.Log(name + " receives force of " + speedForce);
+
+        if(speedForce > minimumForceToBeDestroyed)
+        {
+            destroyed = true;
+            DestroyObject();
+        }
+
+        return destroyed;
+    }
+
+    public virtual void DestroyObject()
+    {
+        Debug.Log(name + " destroyed because of speed");
+        Destroy(gameObject);
+    }
+}
+
+public enum PhysicalObjectInteractionsType
+{
+    None,
+    OnlyDealDamages,
+    OnlyReceiveDamages,
+    DealAndReceiveDamages
+}
