f1c31a6550f676f4d13baad67c2212891e778e1e
Pandanym <nympanda@gmail.com>
2019-11-20 16:44:57 +0100

    HandController Prefab

---
+++ Assets/SpawnGameobjectOnTouch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+
+#if UNITY_EDITOR
+// Set up touch input propagation while using Instant Preview in the editor.
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class SpawnGameobjectOnTouch : MonoBehaviour
+{
+    public GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Should not handle input if the player is pointing on UI.
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            GameObject newGo = Instantiate(go);
+            newGo.transform.position = hit.Pose.position;
+            newGo.transform.localScale *= 0.1f;
+        }
+    }
+}
