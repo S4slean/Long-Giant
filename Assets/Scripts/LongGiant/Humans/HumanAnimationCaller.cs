6b1e004d9a9250e90fb84fea21c1d9c3b2d5cba7
Sebastien <sebastien.butor@orange.fr>
2019-11-22 15:53:01 +0100

    Human Animation Bases

---
+++ Assets/Scripts/LongGiant/Humans/HumanAnimationCaller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanAnimationCaller : MonoBehaviour
+{
+    [SerializeField] HumanScript relatedHuman = default;
+
+    public void LaunchTrueAttack()
+    {
+        relatedHuman.LaunchTrueAttack();
+    }
+}
